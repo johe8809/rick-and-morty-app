5633b4e993414988423687a1840bcb51197c79a9
Yonnathan Henao <johe8809@gmail.com>
2023-09-16 01:13:54 -0500

    update folder structure

---
+++ lib/src/modules/auth/ui/views/login_view.dart
@@ -0,0 +1,15 @@
+part of com.rick_and_morty.app.auth.ui.views;
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/src/modules/dashboard/core/core.dart
@@ -0,0 +1,4 @@
+library com.rick_and_morty.app.dashboard.core;
+
+export './services/services.dart';
+export './state/state.dart';
